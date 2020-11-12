3e7fe2f0a0fc1ed123ee37831c831c2c57b1b417
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-12 18:05:38 +0200

    CameraManager: Fixup exposing aux camera to apps
    Since when are we doing stuff like this?
    Save some runtime, even if a bit. Also look cleaner.
    eb6ebc0c30d1ad582189c2d08d9f683ce18d8e64
    
    Change-Id: Ic8c5844321a7310c2b72e1d7c2eceb5e3e8285e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -2282,12 +2282,12 @@ public final class CameraManager {
             }
         }
 
-        private String[] extractCameraIdListLocked(int deviceId, int devicePolicy) {
-            List<String> cameraIds = new ArrayList<>();
-            boolean exposeAuxCamera = shouldExposeAuxCamera();
+        private String[] extractCameraIdListLocked() {
+            String[] cameraIds = null;
+            boolean exposeAuxCamera = Camera.shouldExposeAuxCamera();
+            int size = exposeAuxCamera ? mDeviceStatus.size() : 2;
             int idCount = 0;
-            for (int i = 0; i < mDeviceStatus.size(); i++) {
-                if(!exposeAuxCamera && (i == 2)) break;
+            for (int i = 0; i < size; i++) {
                 int status = mDeviceStatus.valueAt(i);
                 DeviceCameraInfo info = mDeviceStatus.keyAt(i);
                 if (status == ICameraServiceListener.STATUS_NOT_PRESENT
@@ -2297,7 +2297,16 @@ public final class CameraManager {
                 }
                 cameraIds.add(info.mCameraId);
             }
-            return cameraIds.toArray(new String[0]);
+            cameraIds = new String[idCount];
+            idCount = 0;
+            for (int i = 0; i < size; i++) {
+                int status = mDeviceStatus.valueAt(i);
+                if (status == ICameraServiceListener.STATUS_NOT_PRESENT
+                        || status == ICameraServiceListener.STATUS_ENUMERATING) continue;
+                cameraIds[idCount] = mDeviceStatus.keyAt(i);
+                idCount++;
+            }
+            return cameraIds;
         }
 
         private Set<Set<String>> extractConcurrentCameraIdListLocked(int deviceId,
